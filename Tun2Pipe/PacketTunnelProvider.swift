ef75ef0f5ddddb0904dfa83f043e2564ee84eebc
ribencong <ribencong@outlook.com>
2019-05-31 21:10:32 +0800

    refactoring

--- Tun2Pipe/PacketTunnelProvider.swift
+++ Tun2Pipe/PacketTunnelProvider.swift
@@ -7,103 +7,132 @@
 //
 
 import NetworkExtension
-
-let proxyPort = 51080
+var Tunnel: PacketTunnelProvider?
 
 class PacketTunnelProvider: NEPacketTunnelProvider {
-        var proxy:HttpProxy?
         
-        override func startTunnel(options: [String : NSObject]?, completionHandler: @escaping (Error?) -> Void) {
-                NSLog("---=>:Tunnel start......")
-                self.proxy = HttpProxy(host: "127.0.0.1", port: Int32(proxyPort))
-                
-                let networkSettings = newPacketTunnelSettings(proxyHost: "127.0.0.1", proxyPort: UInt16(proxyPort))
-                
-                setTunnelNetworkSettings(networkSettings){
-                        err in
-                        guard err == nil else{
-                                NSLog("---=>:SetTunnelNetworkSettings err:%s", err.debugDescription)
-                                completionHandler(err)
+        var httpProxy: HTTPProxyServer?
+        
+        var tcpProxy: TCPProxyServer?
+        
+        override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+                Tunnel = self
+                /* http */
+                self.httpProxy = HTTPProxyServer()
+                self.httpProxy!.start(with: "localhost")
+                /* settings */
+                let tunnelSettings: NEPacketTunnelNetworkSettings = self.newPacketTunnelSettings(
+                        proxyHost: self.httpProxy!.listenSocket.localHost!,
+                        proxyPort: self.httpProxy!.listenSocket.localPort
+                )
+                /* tcp */
+                self.tcpProxy = TCPProxyServer()
+                self.tcpProxy!.server.ipv4Setting(
+                        withAddress: tunnelSettings.ipv4Settings!.addresses[0],
+                        netmask: tunnelSettings.ipv4Settings!.subnetMasks[0]
+                )
+                self.tcpProxy!.server.mtu(tunnelSettings.mtu!.uint16Value) { datas, numbers in
+                        guard
+                                let _datas: [Data] = datas,
+                                let _nums: [NSNumber] = numbers
+                                else
+                        {
                                 return
                         }
-                        
-                        completionHandler(nil)
-                        
-                        NSLog("---=>:Tunnel start success......")
+                        self.packetFlow.writePackets(_datas, withProtocols: _nums)
+                }
+                /* start */
+                self.setTunnelNetworkSettings(tunnelSettings) { err in
+                        completionHandler(err)
+                        if err == nil {
+                                self.packetFlow.readPackets() { datas, nums in
+                                        self.handlePackets(packets: datas, protocols: nums)
+                                }
+                        }
                 }
         }
         
         override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-                NSLog("Packet tunnel stopTunnel......")
                 completionHandler()
-                exit(EXIT_SUCCESS)
         }
         
-        func handlePackets() {
-                
-                self.packetFlow.readPackets {
-                        packets, pro in
-                        
-                        for (_, pd) in packets.enumerated(){
-                                NSLog("pakcet:\(pd.count)")
+        override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)? = nil) {
+                guard let message: String = String.init(data: messageData, encoding: .ascii) else {
+                        completionHandler?(nil)
+                        return
+                }
+                switch message {
+                case "getSessionsData":
+                        SessionManager.shared.getSessionsData() { data in
+                                completionHandler?(data)
                         }
-                        
-//                        self.handlePackets()
+                default:
+                        completionHandler?(nil)
                 }
         }
-}
-
-func newPacketTunnelSettings(proxyHost: String, proxyPort: UInt16) -> NEPacketTunnelNetworkSettings {
         
-        let settings: NEPacketTunnelNetworkSettings = NEPacketTunnelNetworkSettings(
-                tunnelRemoteAddress: "240.0.0.1"
-        )
-        /* MTU */
-        settings.mtu = NSNumber(value: UINT16_MAX)
+        func handlePackets(packets: [Data], protocols: [NSNumber]) {
+                for (index, data) in packets.enumerated() {
+                        switch protocols[index].int32Value {
+                        case AF_INET:
+                                self.tcpProxy?.server.ipPacketInput(data)
+                        case AF_INET6:
+                                break
+                        default:
+                                fatalError()
+                        }
+                }
+                self.packetFlow.readPackets { datas, numbers in
+                        self.handlePackets(packets: datas, protocols: numbers)
+                }
+        }
         
-        /* proxy settings */
-        let proxySettings: NEProxySettings = NEProxySettings()
+        func newPacketTunnelSettings(proxyHost: String, proxyPort: UInt16) -> NEPacketTunnelNetworkSettings {
+                let settings: NEPacketTunnelNetworkSettings = NEPacketTunnelNetworkSettings(
+                        tunnelRemoteAddress: "240.0.0.1"
+                )
+                
+                /* proxy settings */
+                let proxySettings: NEProxySettings = NEProxySettings()
+                proxySettings.httpServer = NEProxyServer(
+                        address: proxyHost,
+                        port: Int(proxyPort)
+                )
+                proxySettings.httpsServer = NEProxyServer(
+                        address: proxyHost,
+                        port: Int(proxyPort)
+                )
+                proxySettings.autoProxyConfigurationEnabled = false
+                proxySettings.httpEnabled = true
+                proxySettings.httpsEnabled = true
+                proxySettings.excludeSimpleHostnames = true
+                proxySettings.exceptionList = [
+                        "192.168.0.0/16",
+                        "10.0.0.0/8",
+                        "172.16.0.0/12",
+                        "127.0.0.1",
+                        "localhost",
+                        "*.local"
+                ]
+                settings.proxySettings = proxySettings
+                
+                /* ipv4 settings */
+                let ipv4Settings: NEIPv4Settings = NEIPv4Settings(
+                        addresses: [settings.tunnelRemoteAddress],
+                        subnetMasks: ["255.255.255.255"]
+                )
+                        ipv4Settings.includedRoutes = [NEIPv4Route.default()]
+                ipv4Settings.excludedRoutes = [
+                        NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
+                        NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
+                        NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0")
+                ]
+                settings.ipv4Settings = ipv4Settings
+                
+                /* MTU */
+                settings.mtu = NSNumber(value: UINT16_MAX)
+                
+                return settings
+        }
         
-//        proxySettings.autoProxyConfigurationEnabled = true
-//        proxySettings.proxyAutoConfigurationJavaScript="function FindProxyForURL(url, host) {return \"SOCKS5 127.0.0.1:51080; SOCKS 127.0.0.1:51080; DIRECT;\";}"
-//        proxySettings.excludeSimpleHostnames = true;
-        
-        proxySettings.httpServer = NEProxyServer(
-                address: proxyHost,
-                port: Int(proxyPort)
-        )
-        proxySettings.httpsServer = NEProxyServer(
-                address: proxyHost,
-                port: Int(proxyPort)
-        )
-        proxySettings.autoProxyConfigurationEnabled = false
-        proxySettings.httpEnabled = true
-        proxySettings.httpsEnabled = true
-        proxySettings.excludeSimpleHostnames = true
-        proxySettings.exceptionList = [
-                "192.168.0.0/16",
-                "10.0.0.0/8",
-                "172.16.0.0/12",
-                "127.0.0.1",
-                "localhost",
-                "*.local"
-        ]
-//        let doms = PacDomain.shared.cache
-        proxySettings.matchDomains = [""]
-        settings.proxySettings = proxySettings
-        
-        /* ipv4 settings */
-        let ipv4Settings: NEIPv4Settings = NEIPv4Settings(
-                addresses: ["10.8.0.2"],
-                subnetMasks: ["255.255.255.255"]
-        )
-//        ipv4Settings.includedRoutes = [NEIPv4Route.default()]
-//        ipv4Settings.excludedRoutes = [
-//                NEIPv4Route(destinationAddress: "192.168.0.0", subnetMask: "255.255.0.0"),
-//                NEIPv4Route(destinationAddress: "10.0.0.0", subnetMask: "255.0.0.0"),
-//                NEIPv4Route(destinationAddress: "172.16.0.0", subnetMask: "255.240.0.0")
-//        ]
-        settings.ipv4Settings = ipv4Settings
-        
-        return settings
 }
