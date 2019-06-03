ce0276c76841dfaa21f1d261c2308f1594539718
ribencong <ribencong@outlook.com>
2019-06-03 23:45:16 +0800

    refactoring

--- TunProxy/Pipe.swift
+++ TunProxy/Pipe.swift
@@ -18,8 +18,14 @@ public struct HTTPData {
 }
 
 public enum PipeStatus: Int {
-        case invalid = 0, readFirstHead = 1, readContent, sendingConnectResponse,
-        readingContent, forwarding
+        case invalid = 0,
+        
+        ProxyGetTarget = 1,
+        ProxyConnectResponse,
+        ProxyWriteBackResponse,
+        
+        AdapterWriteOut,
+        AdapterWaitResponse
 }
 
 class Pipe: NSObject{
@@ -41,28 +47,32 @@ class Pipe: NSObject{
                 self.proxySock.delegate = self
                 self.proxySock.readData(to: HTTPData.DoubleCRLF,
                                         withTimeout: -1,
-                                        tag: PipeStatus.readFirstHead.rawValue)
+                                        tag: PipeStatus.ProxyGetTarget.rawValue)
         }
         
         func Close() {
                 self.CCB?()
                 self.proxySock.disconnect()
+                self.adapterSock?.disconnect()
         }
 }
 
 extension Pipe: GCDAsyncSocketDelegate{
         
         open func socket(_ sock: GCDAsyncSocket, didRead data: Data, withTag tag: Int) {
-                
                 do {
                         switch (PipeStatus.init(rawValue: tag))!{
-                        case .readFirstHead:
+                        case .ProxyGetTarget:
                                 let header = try HTTPHeader(headerData: data)
                                 try self.OpenAdapter(header: header)
-                        case .forwarding:
-                                NSLog("forwarding......")
+                        case .AdapterWaitResponse:
+                                NSLog("------=>:AdapterWaitResponse......\(data.count)")
+                                self.proxySock.write(data,
+                                                     withTimeout: -1,
+                                                     tag: PipeStatus.ProxyWriteBackResponse.rawValue)
                                 break;
                         default:
+                                NSLog("------=>:didRead unknown......")
                                 return
                         }
                         
@@ -76,50 +86,58 @@ extension Pipe: GCDAsyncSocketDelegate{
         open func socket(_ sock: GCDAsyncSocket, didConnectToHost host: String, port: UInt16) {
                 
                 NSLog("------>Pipe:didConnectToHost:\(host):\(port)")
-                
-                guard sock == self.adapterSock else {
-                        NSLog("It should be adapter socket connected")
-                        exit(EXIT_FAILURE)
-                }
-                
+
                 if self.isConnect{
                         self.proxySock.write(HTTPData.ConnectSuccessResponse,
                                              withTimeout: -1,
-                                             tag: PipeStatus.sendingConnectResponse.rawValue)
+                                             tag: PipeStatus.ProxyConnectResponse.rawValue)
                 }else{
                         self.adapterSock!.write(self.firstHeaderData!,
                                                 withTimeout: -1,
-                                                tag: PipeStatus.readingContent.rawValue)
+                                                tag: PipeStatus.AdapterWriteOut.rawValue)
                         
-                        self.adapterSock?.readData(withTimeout: -1, tag: PipeStatus.forwarding.rawValue)
+                        self.adapterSock?.readData(withTimeout: -1,
+                                                   tag: PipeStatus.AdapterWaitResponse.rawValue)
                 }
         }
         
         open func socket(_ sock: GCDAsyncSocket, didWriteDataWithTag tag: Int) {
+                
                 switch (PipeStatus.init(rawValue: tag))! {
-                case .sendingConnectResponse:
-                        NSLog("sendingConnectResponse......")
+                case .ProxyConnectResponse:
+                        NSLog("------=>:ProxyConnectResponse......")
                         break
-                case .readingContent:
-                        NSLog("sendingConnectResponse......")
+                case .AdapterWriteOut:
+                        NSLog("------=>:AdapterWriteOut......")
+                        break
+                case .ProxyWriteBackResponse:
+                        NSLog("------=>:ProxyWriteBackResponse......")
                         break
                 default:
+                        NSLog("------=>:didWriteDataWithTag unknown......")
                         return
                 }
         }
         
         open func socketDidDisconnect(_ socket: GCDAsyncSocket, withError err: Error?) {
-                NSLog("socketDidDisconnect......\(err.debugDescription)")
+                NSLog("------=>:socketDidDisconnect......\(err.debugDescription)")
+                self.Close()
         }
-        
 }
 
 extension Pipe {
         
         func OpenAdapter(header:HTTPHeader) throws {
+                
+                if header.isConnect{
+                        NSLog("------=>:暂时不处理, 方便测试:\(header.toString())")
+                        throw YPError.SystemError
+                }
+                
                 self.targetAddr = header.host
                 self.targetoPort = UInt16(header.port)
                 self.isConnect = header.isConnect
+                
                 if !self.isConnect{
                         header.removeProxyHeader()
                         header.rewriteToRelativePath()
---
+++ TunProxy/YPError.swift
@@ -0,0 +1,12 @@
+//
+//  YPError.swift
+//  TunProxy
+//
+//  Created by wsli on 2019/6/3.
+//  Copyright © 2019 com.ribencong.youPipe. All rights reserved.
+//
+
+import Foundation
+enum YPError:Error{
+        case SystemError, BusinessError
+}
