88998ec0fcb6a71540e27ff7732614b98a87a2f4
ribencong <ribencong@outlook.com>
2019-06-26 19:17:00 +0800

    refactoring

---
+++ DomainParser/DomainParser.h
@@ -0,0 +1,19 @@
+//
+//  DomainParser.h
+//  DomainParser
+//
+//  Created by wsli on 2019/6/26.
+//  Copyright © 2019 com.ribencong.youPipe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DomainParser.
+FOUNDATION_EXPORT double DomainParserVersionNumber;
+
+//! Project version string for DomainParser.
+FOUNDATION_EXPORT const unsigned char DomainParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DomainParser/PublicHeader.h>
+
+
